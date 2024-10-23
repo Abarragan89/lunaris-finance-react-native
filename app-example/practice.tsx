3212332e08bada649fe73a707d8ee01c71f90897
Anthony Barragan <anthony.bar.89@gmail.com>
2024-10-23 12:30:03 -0700

    initial commit after making a few tabs and deleting unnecessary code

---
+++ app-example/practice.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export default function practice() {
+    return (
+        <Text>Hi there, practice man!</Text>
+    )
+}
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export default function settings() {
+    return (
+        <Text>Settings apge</Text>
+    )
+}
